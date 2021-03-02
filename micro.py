a80aa3fb81928ddf5a0b537b8fd2361c8f81d68f
vcwild <vcwildm@gmail.com>
2021-03-01 23:48:05 -0300

    initial commit

---
+++ micro.py
@@ -0,0 +1,41 @@
+import fire
+from parsel import Selector
+from httpx import get
+
+
+class MeuGuia:
+    def categorias(self):
+        response = get('https://meuguia.tv/').text
+
+        s = Selector(response)
+
+        return {'categorias': s.css('h2::text').getall()}
+    
+    def canal(self, channel):
+        request = get(f'https://meuguia.tv/programacao/canal/{channel}')
+
+        if request.status_code == 404:
+            return f'Canal {channel} não encontrado'
+        
+        response = request.text
+        s = Selector(response)
+        
+        return {
+            'canal': s.css('#canal_header .devicepadding::text').get(),
+            'programa': s.css('h2::text').get(), 
+            'início': s.css('div.time::text').get(),
+            'categoria': s.css('h3::text').get(),
+        }
+
+    def categoria(self, category):
+        response = get(f'https://meuguia.tv/programacao/categoria/{category}').text
+
+        s = Selector(response)
+
+        return {'canais': s.css('h2::text').getall()}
+    
+
+
+if __name__ == '__main__':
+    guia = MeuGuia()
+    fire.Fire(guia)
